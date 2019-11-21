26673a8029059787e93936cb6e5767d4e8929a90
David <david999yang@gmail.com>
2019-11-21 19:55:49 +0000

    add linux directory

---
+++ linux_sql/git_master_branch.sh
@@ -0,0 +1,7 @@
+git init
+#use -A option to add all files recursively
+git add -A
+git commit -m "Init project"
+#update git url below 
+git remote add origin git@github.com:jarvisgroup/jarvis_data_eng_demo.git
+git push -u origin master
