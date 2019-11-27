76a95bf6a60e428d79f6da89d8de240fd678942a
David <david999yang@gmail.com>
2019-11-27 15:07:48 +0000

    update

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -8,26 +8,26 @@ DROP TABLE IF EXISTS PUBLIC.host_usage CASCADE;
 CREATE TABLE IF NOT EXISTS PUBLIC.host_info (
 	id SERIAL NOT NULL, 
 	hostname VARCHAR(150) UNIQUE NOT NULL, 
-	cpu_number SMALLINT, 
+	cpu_number SMALLINT NOT NULL, 
 	cpu_architecture VARCHAR(50) NOT NULL, 
 	cpu_model VARCHAR(150) NOT NULL, 
-	cpu_mhz NUMERIC(7, 3), 
-	L2_cache SMALLINT, 
-	total_mem INT,
-	timestamp TIMESTAMP, 
+	cpu_mhz NUMERIC(7, 3) NOT NULL, 
+	L2_cache SMALLINT NOT NULL, 
+	total_mem INT NOT NULL,
+	timestamp TIMESTAMP NOT NULL, 
 				
 	/* Define Primary Key & Foreign Key constraints here */
 	PRIMARY KEY(id)
 );
 				
 CREATE TABLE IF NOT EXISTS PUBLIC.host_usage (
-	timestamp TIMESTAMP, 
-	host_id INT, 
-	memory_free INT, 
-	cpu_idle NUMERIC, 
-	cpu_kernel NUMERIC, 
-	disk_io SMALLINT, 
-	disk_available INT,
+	timestamp TIMESTAMP NOT NULL, 
+	host_id INT NOT NULL, 
+	memory_free INT NOT NULL, 
+	cpu_idle NUMERIC NOT NULL, 
+	cpu_kernel NUMERIC NOT NULL, 
+	disk_io SMALLINT NOT NULL, 
+	disk_available INT NOT NULL,
 	
 	/* Define Primary Key & Foreign Key constraints here */
 	PRIMARY KEY(timestamp, host_id), 
