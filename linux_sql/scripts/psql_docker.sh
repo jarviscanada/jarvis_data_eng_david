4f86b4984046be35127fe94de85e5b3f9e390675
David <david999yang@gmail.com>
2019-11-28 18:00:50 +0000

    add rm and more checking statements

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -4,6 +4,20 @@
 mode=$1
 db_password=$2
 
+
+#define functions
+
+# return 0 if  jrvs-psql grepped, 1 if jrvs-psql not found
+verify_created() {
+	docker container ls -a -f name=jrvs-psql | grep -q 'jrvs-psql'
+}
+
+# return 0 if  jrvs-psql grepped, 1 if jrvs-psql not found
+verify_running() {
+	docker ps -f name=jrvs-psql | grep -q 'jrvs-psql'
+}
+
+
 #validate arguments
 if [ "$#" -lt 1 ]; then
     echo "WARNING: Illegal number of parameters" >&2
@@ -18,24 +32,44 @@ fi
 
 #setup or stop psql docker service
 if [ ${mode,,} = 'start' ]; then
-	if docker ps | grep -q 'jrvs-psql'; then
+	if verify_running; then
 		echo "WARNING: jrvs-psql is already running" >&2
 		exit 1
+	elif verify_created; then
+		echo "STARTING already created container jrvs-psql"
+		docker container start jrvs-psql
+		exit 0
 	fi
-	sudo systemctl status docker >/dev/null || sudo systemctl start docker
+	sudo systemctl status docker > /dev/null 2>&1 || sudo systemctl start docker
 	docker pull postgres
 	export PGPASSWORD=${db_password}
 	docker volume create pgdata
 	docker run --rm --name jrvs-psql -e POSTGRES_PASSWORD=$PGPASSWORD -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres
-	psql -h localhost -U postgres -d host_agent -f /home/centos/dev/jarvis_data_eng_David_Yang/linux_sql/sql/ddl.sql #run ddl scripts to create new  table
-
-else # case stop
-	if  ! docker ps | grep -q 'jrvs-psql'; then
-		echo "WARNING: jrvs-psql is not running" >&2
+	if ! verify_created; then
+		echo "WARNING: jrvs-psql could not be created" >&2
 		exit 1
 	fi
-	docker stop jrvs-psql
-	sudo systemctl stop docker
+	if  verify_running; then
+		psql -h localhost -U postgres -f /home/centos/dev/jarvis_data_eng_David_Yang/linux_sql/sql/ddl.sql #run ddl scripts to create new  table
+		echo "SUCCESSFULLY created database & tables"
+	else
+		echo "WARNING: jrvs-psql instsance not running" >&2
+	fi
+
+else # case stop
+	if  ! verify_running; then
+		echo "WARNING: jrvs-psql is not running" >&2
+	else
+		docker container stop jrvs-psql
+	fi
+
+
+	docker container rm jrvs-psql 2>/dev/null
+	if  ! verify_created; then
+		echo "SUCCESSFULLY stopped jrvs-psql container"
+	else
+		echo "WARNING: attempt to stop jrvs-psql container failed" >&2
+	fi
 fi
 
 exit 0
