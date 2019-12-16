5e6cfc058e9deb560f86921214dbdbf5dac4243e
David Yang <yangdav9@localhost.localdomain>
2019-12-16 11:49:12 -0500

    readd

---
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -0,0 +1,29 @@
+package ca.jrvs.apps.jdbc;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+import javax.swing.JOptionPane;
+
+public class JDBCExecutor {
+
+  public static void main (String[] args) {
+
+    String url = new String("jdbc:postgresql://localhost:5432/hplussport");
+    try (Connection connection = DriverManager.getConnection(url,
+        "postgres",
+        "password");){
+      OrderDAO orderDAO = new OrderDAO(connection);
+      Order order = orderDAO.findById(1000);
+      System.out.println(order);
+
+    }catch(SQLException e){
+      e.printStackTrace();
+      throw new RuntimeException(e);
+    }
+  }
+}
+
+
