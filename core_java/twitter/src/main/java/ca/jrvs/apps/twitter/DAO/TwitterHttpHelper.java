4c6ac99bd5f7b754c86afa065ba0100b554299c1
David Yang <yangdav9@localhost.localdomain>
2019-12-16 13:25:46 -0500

    add changes

---
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/DAO/TwitterHttpHelper.java
@@ -0,0 +1,17 @@
+package ca.jrvs.apps.twitter.DAO;
+
+import java.net.URI;
+import org.apache.http.HttpResponse;
+
+public class TwitterHttpHelper implements HttpHelper {
+
+  @Override
+  public HttpResponse httpPost(URI uri) {
+    return null;
+  }
+
+  @Override
+  public HttpResponse httpGet(URI uri) {
+    return null;
+  }
+}
