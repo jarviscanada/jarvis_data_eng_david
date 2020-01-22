c3191d625374d5753a4a78ba4ce76a82a402731d
David Yang <yangdav9@localhost.localdomain>
2020-01-22 14:32:33 -0500

    update service

--- springboot/src/main/java/ca/jrvs/apps/trading/service/TraderAccountService.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/service/TraderAccountService.java
@@ -5,7 +5,6 @@ import ca.jrvs.apps.trading.model.*;
 import java.lang.reflect.Field;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
-import java.util.ArrayList;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
