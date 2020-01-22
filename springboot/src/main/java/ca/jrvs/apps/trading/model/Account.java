fef091eb2110eae66b117b2cd0dd960250018455
David Yang <yangdav9@localhost.localdomain>
2020-01-22 14:02:37 -0500

    update models

--- springboot/src/main/java/ca/jrvs/apps/trading/model/Account.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/model/Account.java
@@ -1,5 +1,62 @@
 package ca.jrvs.apps.trading.model;
 
-public class Account {
+import ca.jrvs.apps.trading.model.domain.Entity;
+import java.util.Objects;
 
+public class Account implements Entity<Integer> {
+  private Integer id;
+  private Integer trader_id;
+  private Double amount;
+
+  public Account() {
+    this.id = null;
+    this.trader_id = null;
+    this.amount = null;
+  }
+
+  public Double getAmount() { return amount; }
+
+  public void setAmount(Double amount) { this.amount = amount; }
+
+  public Integer getTrader_id() {
+    return trader_id;
+  }
+
+  public void setTrader_id(Integer trader_id) {
+    this.trader_id = trader_id;
+  }
+
+  @Override
+  public Integer getId() { return id; }
+
+  @Override
+  public void setId(Integer id) { this.id = id; }
+
+  @Override
+  public String toString() {
+    return "Account{" +
+        "id=" + id +
+        ", trader_id=" + trader_id +
+        ", amount=" + amount +
+        '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Account account = (Account) o;
+    return Objects.equals(id, account.id) &&
+        Objects.equals(trader_id, account.trader_id) &&
+        Objects.equals(amount, account.amount);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, trader_id, amount);
+  }
 }
--- springboot/src/main/java/ca/jrvs/apps/trading/model/Position.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/model/Position.java
@@ -1,5 +1,50 @@
 package ca.jrvs.apps.trading.model;
 
-public class Position {
+import ca.jrvs.apps.trading.model.domain.Entity;
 
+public class Position implements Entity<Integer> {
+  private Integer id;
+  private String ticker;
+  private Integer position;
+
+  public Position() {
+    this.id = null;
+    this.ticker = null;
+    this.position = null;
+  }
+
+  @Override
+  public Integer getId() {
+    return id;
+  }
+
+  @Override
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getTicker() {
+    return ticker;
+  }
+
+  public void setTicker(String ticker) {
+    this.ticker = ticker;
+  }
+
+  public Integer getPosition() {
+    return position;
+  }
+
+  public void setPosition(Integer position) {
+    this.position = position;
+  }
+
+  @Override
+  public String toString() {
+    return "Position{" +
+        "id=" + id +
+        ", ticker='" + ticker + '\'' +
+        ", position=" + position +
+        '}';
+  }
 }
