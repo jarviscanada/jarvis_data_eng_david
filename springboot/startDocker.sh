0c44ed3bd302296b851018b8fc709db511ecc379
David Yang <yangdav9@localhost.localdomain>
2020-02-06 13:55:11 -0500

    update script

--- springboot/startDocker.sh
+++ springboot/startDocker.sh
@@ -5,6 +5,7 @@ sudo systemctl status docker > /dev/null 2>&1 || sudo systemctl start docker
 #create a new docker network
 docker network create --driver bridge trading-net
 
+docker build -t trading-psql ./psql 
 docker build -t trading-app . 
 docker image ls -f reference=trading-psl
 
@@ -13,26 +14,26 @@ docker image ls -f reference=trading-psl
 
 docker volume create sbdata
 
-docker run --name trading-psql-demo-local \
+docker run --name trading-psql \
 -e POSTGRES_PASSWORD=password \
 -e POSTGRES_DB=jrvstrading \
 -e POSTGRES_USER=postgres \
 -v sbdata:/var/lib/postgresql/data \
 --network trading-net \
--d -p 5432:5432 jrvs/trading-psql-demo
+-d -p 5432:5432 trading-psql
 
 
 
 #start trading-app container which is attached to the trading-net docker network
 docker run -d --name trading-app-local \
--e "PSQL_URL=jdbc:postgresql://trading-psql-demo-local:5432/jrvstrading" \
+-e "PSQL_URL=jdbc:postgresql://trading-psql:5432/jrvstrading" \
 -e "PSQL_USER=postgres" \
 -e "PSQL_PASSWORD=password" \
 -e "IEX_PUB_TOKEN=${IEX_PUB_TOKEN}" \
 --network trading-net \
 -p 8080:8080 -t trading-app
 
-docker container start trading-psql-demo-local
+docker container start trading-psql
 docker container start trading-app-local
 
 docker ps
